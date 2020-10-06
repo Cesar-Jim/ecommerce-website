d8c48d454cb55427d9485294d26071bd1bf3757d
Cesar Jiménez <cijgr@hotmail.com>
2020-10-05 21:41:48 -0500

    added homepage functional component with styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Homepage from './homepage.component';
+
 import './App.css';
+import HomePage from './homepage.component';
 
 function App() {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;

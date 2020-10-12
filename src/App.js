2ace4717d9c026ccf66d7e5e41477775af77a0b7
Cesar Jiménez <cijgr@hotmail.com>
2020-10-11 20:55:11 -0500

    created the shop page, collection preview and menu item components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-// Switch provides more control over the routing
+import { Switch, Route } from 'react-router-dom'; // Switch provides more control over the routing
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 import './App.css';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact={true} path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route exact={true} path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );

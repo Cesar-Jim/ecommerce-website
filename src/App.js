8c9f0ed3f33edd34be6d173b518f38745acac6f2
Cesar Jiménez <cijgr@hotmail.com>
2020-10-18 20:40:05 -0500

    Added styled sign-in component with flexible input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'; // Switch provides more control over the routing
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
+import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import './App.css';
+import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Switch>
         <Route exact={true} path='/' component={HomePage} />
         <Route exact={true} path='/shop' component={ShopPage} />
+        <Route exact={true} path='/sign-in' component={SignInSignUpPage} />
       </Switch>
     </div>
   );
